ecd3729302fb073602f5c34bde8e9cd04c6a76b1
arswe <abdurrahmansoftw@gmail.com>
2024-03-25 01:17:51 +0600

    JwtModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
-import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtModule],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
