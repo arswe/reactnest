981c9b18ae68fe4e6679b4c15c1d1b9908793366
arswe <abdurrahmansoftw@gmail.com>
2024-03-25 01:58:50 +0600

    return payload

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt } from 'passport-jwt';
 import { Strategy } from 'passport-local';
+import { TokenPayload } from './../../../dist/auth/token-payload.enti.d';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors,
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request.cookies.Authentication,
+      ]),
+      secretOrKey: configService.getOrThrow('JWT_SECRET'),
     });
+  }
+  validate(payload: TokenPayload) {
+    return payload;
   }
 }
