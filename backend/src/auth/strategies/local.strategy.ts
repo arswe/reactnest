9fdb0c9020530e464df50c071faf32cb230d94ad
arswe <abdurrahmansoftw@gmail.com>
2024-03-25 00:59:19 +0600

    validate

--- backend/src/auth/strategies/local.strategy.ts
+++ backend/src/auth/strategies/local.strategy.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { UsersService } from './../../users/users.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersService: UsersService) {
     super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string) {
-    return { email, password };
+    try {
+      return await this.usersService.verifyUser(email, password);
+    } catch (error) {
+      return null;
+    }
   }
 }
