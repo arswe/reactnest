701b84385f77e999cbc58d595ffe046950f75985
arswe <abdurrahmansoftw@gmail.com>
2024-03-25 00:00:37 +0600

    API_URL

--- frontend/src/services/utils/urls.ts
+++ frontend/src/services/utils/urls.ts
@@ -1,4 +1,3 @@
-const API_URL: string = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:3000'
+const API_URL: string = import.meta.env.REACT_APP_API_URL || 'http://localhost:3000'
 
 export { API_URL }
-
