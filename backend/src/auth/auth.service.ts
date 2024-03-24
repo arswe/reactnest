83507354b75628465a4efc8bc2ee43859dc7eea3
arswe <abdurrahmansoftw@gmail.com>
2024-03-25 01:32:24 +0600

    Authentication

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
 import { User } from 'src/users/entities/user.entity';
+import { TokenPayload } from './../../dist/auth/token-payload.interface.d';
 
 @Injectable()
 export class AuthService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    private readonly jwtSerive: JwtService,
+  ) {}
 
   async login(user: User, response: Response) {
     const expiresIn = new Date();
     expiresIn.setSeconds(
       expiresIn.getSeconds() + this.configService.getOrThrow('JWT_EXPIRATION'),
     );
+
+    const tokenPayload: TokenPayload = {
+      _id: user._id.toHexString(),
+      email: user.email,
+    };
+    const token = this.jwtSerive.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires: expiresIn,
+      secure: this.configService.getOrThrow('HTTPS'),
+      sameSite: 'none',
+    });
   }
 }
