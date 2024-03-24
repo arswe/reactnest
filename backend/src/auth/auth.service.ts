94b0ab308e9cb445ba1a2bb702f81397828568a3
arswe <abdurrahmansoftw@gmail.com>
2024-03-25 01:25:38 +0600

    AuthService

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private configService: ConfigService) {}
+
+  async login(user: User, response: Response) {
+    const expiresIn = new Date();
+    expiresIn.setSeconds(
+      expiresIn.getSeconds() + this.configService.getOrThrow('JWT_EXPIRATION'),
+    );
+  }
+}
