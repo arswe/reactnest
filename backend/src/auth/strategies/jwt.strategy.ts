4bae5c271dffe63347d4b88670de70b5711b618d
arswe <abdurrahmansoftw@gmail.com>
2024-03-25 01:53:06 +0600

    JwtStrategy

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt,
+    });
+  }
+}
