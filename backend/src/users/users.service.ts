8910d6604905177f6cedbbeca5f9c8885a717564
arswe <abdurrahmansoftw@gmail.com>
2024-03-24 12:45:06 +0600

    update

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -28,15 +28,15 @@ export class UsersService {
   }
 
   async update(_id: string, updateUserInput: UpdateUserInput) {
-    const password = updateUserInput.password ?? '';
+    if (updateUserInput.password) {
+      updateUserInput.password = await this.hashPassword(
+        updateUserInput.password,
+      );
+    }
+
     return this.userRepository.findOneAndUpdate(
       { _id },
-      {
-        $set: {
-          ...updateUserInput,
-          password: await this.hashPassword(password),
-        },
-      },
+      { $set: { ...updateUserInput } },
     );
   }
 
