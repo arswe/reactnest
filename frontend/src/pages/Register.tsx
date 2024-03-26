67392ac573982f77de2d6dac3d7128f09ccaf3ac
arswe <abdurrahmansoftw@gmail.com>
2024-03-26 12:58:36 +0600

    RegisterProps

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -4,30 +4,33 @@ import { useCreateUser } from '../hooks/useCreateUser'
 import Auth from '../layouts/Auth'
 import { extractErrorMessage } from '../services/utils/errors'
 
+interface RegisterProps {
+  email: string
+  password: string
+}
+
 const Register = () => {
   const [createUser] = useCreateUser()
   const [error, setError] = useState('')
 
-  return (
-    <Auth
-      submitLabel='Register'
-      error={error}
-      onSubmit={async ({ email, password }) => {
-        try {
-          await createUser({
-            variables: { createUserInput: { email, password } },
-          })
+  const haandelSubmit = async ({ email, password }: RegisterProps) => {
+    try {
+      await createUser({
+        variables: { createUserInput: { email, password } },
+      })
 
-          setError('')
-        } catch (err) {
-          const errorMessage = extractErrorMessage(err)
-          if (errorMessage) {
-            setError(errorMessage)
-            return
-          }
-        }
-      }}
-    >
+      setError('')
+    } catch (err) {
+      const errorMessage = extractErrorMessage(err)
+      if (errorMessage) {
+        setError(errorMessage)
+        return
+      }
+    }
+  }
+
+  return (
+    <Auth submitLabel='Register' error={error} onSubmit={haandelSubmit}>
       <Link to={'/login'}>Login</Link>
     </Auth>
   )
