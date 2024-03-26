1d85c69b9aa17a26c002b27dff6526b0908b5063
arswe <abdurrahmansoftw@gmail.com>
2024-03-26 13:03:31 +0600

    LoginRequest

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -8,7 +8,7 @@ interface LoginRequest {
 }
 
 const useLogin = () => {
-  const [error, setError] = useState<boolean>()
+  const [error, setError] = useState<string>()
 
   const login = async (request: LoginRequest) => {
     const res = await fetch(`${API_URL}/auth/login`, {
@@ -17,13 +17,18 @@ const useLogin = () => {
       body: JSON.stringify(request),
     })
     if (!res.ok) {
-      setError(true)
+      if (res.status === 401) {
+        setError('Credentials are not valid.')
+      } else {
+        setError('Unknown error occured.')
+      }
       return
     }
-    setError(false)
+    setError('')
     await client.refetchQueries({ include: 'active' })
   }
+
   return { login, error }
 }
 
-export default useLogin
+export { useLogin }
