3cf0a68cd2eb20e9e3c4fb077d6dba0da7732aa4
arswe <abdurrahmansoftw@gmail.com>
2024-03-26 13:26:21 +0600

    GuardProps

--- frontend/src/services/guard/Guard.tsx
+++ frontend/src/services/guard/Guard.tsx
@@ -1,7 +1,12 @@
+import { useGetMe } from '../../hooks/useGetMe'
+
 interface GuardProps {
   children: JSX.Element
 }
 
-const Guard = ({ children }: GuardProps) => {}
+const Guard = ({ children }: GuardProps) => {
+  const { data: user } = useGetMe()
+  console.log(user)
+}
 
 export default Guard
